33605ce1b4a978678dbe3b79e0f691e5a4e74b4b
Adam Butterfield <adb707@yahoo.com>
2018-04-06 14:57:04 +0900

    Update instructions

--- index.js
+++ index.js
@@ -5,14 +5,15 @@
     1. Submit a long URL into the input form, and return a shorter URL.
       ex: http://some-domain.com/some-looooooooog-url-that-is-really-really-long-and-stuff
           -> http://localhost:1337/short-url-afda8
-    2. Make the application redirect from the short URL to the full URL.
+    2. Do not create a short url if the submitted input is empty.
+    3. Make the application redirect from the short URL to the full URL.
 
   Extra:
     1. Check if the long URL has already been submitted, and return the short
        URL directy instead of generating a new one.
     2. Validate that a short URL is unique before saving it.
-    3. Save the data to a database or write it to a file, so that the data
-       persists even if you restart the server.
+    3. Save the data by writing it to a file, so that the data persists even
+       if you restart the server.
     4. Submit the form asynchronously so the page doesn't have to refrest to show
        the short URL.
  */
